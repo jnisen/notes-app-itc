4fd167217baf6e8bed13a10486b5212de7703a9c
jnisen <jnisen@gmail.com>
2021-10-08 09:19:10 +0300

    conditional title on notes

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 class AddNote extends React.Component {
     constructor(props) {
         super(props)
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -5,13 +5,13 @@ const Notes = ({ note, handleDeleteNote }) => {
     const { id, text, date, title } = note
     return (
         <div className="note">
-            <h2>Title: {title}</h2>
+            <h2>{(title.length > 0) ? `Title: ${title}` : ''}</h2>
             <span>{text}</span>
             <div className="note-footer">
                 <small>{date}</small>
                 <MdDeleteForever className="delete-icon" size='1.3em' onClick={() => {
                     (window.confirm('Are you sure you want to delete this note')) ?
-                    handleDeleteNote(id) : alert('Deleted Cancelled')
+                        handleDeleteNote(id) : alert('Deleted Cancelled')
                 }} />
             </div>
         </div>
